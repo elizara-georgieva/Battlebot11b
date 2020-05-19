cae9de79c09f1843af17090000e75e7470db7823
viko <vikotodorov02@gmail.com>
2020-05-19 16:58:39 +0300

    add code for esp

---
+++ Firmware/ESPcode.ino
@@ -0,0 +1,30 @@
+#include "ESP8266WiFi.h"
+
+const char*  ssid = "golqmataMama";
+const char* psw = "123456789";
+
+// create servet at port 9999
+WiFiServer server(9999);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  while(!WiFi.softAP(ssid, psw));
+  delay(1000);
+  server.begin();
+}
+
+void loop() {
+   WiFiClient client = server.available();
+
+   if (client) {
+      while(client.connected()) {
+        while(client.available()>0) {
+          char c = client.read();
+          Serial.write(c);
+        }
+        delay(1000);
+      }
+   }
+   client.stop();
+}
