29a7f6f4ab8feeefd556b408a2f4169b9dddc3ba
Viko <vikotodorov02@gmail.com>
2020-05-21 02:39:23 +0300

    adding some meaningful names and socket functionality

--- App/main.py
+++ App/main.py
@@ -1,27 +1,38 @@
-import pygame
 import sys
-import time
-import manage_input
+import socket
+
+import pygame
+
+from manage_input import convert_list
 
 pygame.init()
-main = True
+flag = True
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 array = []
 
+# create TCP socket
+# connect it to default IP, whick is 192.168.4.1
+# when ESP is used like softAP on port 9999
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '192.168.4.1'
+port = 9999
+sock.connect((host, port))
 
-while main == True:
+while flag:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit(); sys.exit()
-            main = False
+            pygame.quit()
+            sys.exit()
+            sock.close()
+            flag = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == ord('w'):
                 array.append(1)
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 array.append(2)
-            if  event.key == pygame.K_RIGHT or event.key == ord('d'):
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 array.append(3)
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 array.append(4)
@@ -31,7 +42,7 @@ while main == True:
                 array.remove(1)
             if event.key == pygame.K_LEFT or event.key == ord('a'):
                 array.remove(2)
-            if  event.key == pygame.K_RIGHT or event.key == ord('d'):
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 array.remove(3)
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 array.remove(4)
@@ -40,5 +51,4 @@ while main == True:
                 sys.exit()
                 main = False
     pygame.time.wait(250)
-    print(array)
-    manage_input.send_data(array)
+    sock.send(convert_list(array))
--- App/manage_input.py
+++ App/manage_input.py
@@ -1,19 +1,21 @@
-def send_data(array):
+def convert_list(array):
     if 1 in array and len(array) == 1:
-        data = 1
+        data = '1'
     elif 2 in array and len(array) == 1:
-        data = 2
+        data = '2'
     elif 3 in array and len(array) == 1:
-        data = 3
+        data = '3'
     elif 4 in array and len(array) == 1:
-        data = 4
+        data = '4'
     elif 1 in array and 2 in array and len(array) == 2:
-        data = 5
+        data = '5'
     elif 1 in array and 3 in array and len(array) == 2:
-        data = 6
+        data = '6'
     elif 4 in array and 2 in array and len(array) == 2:
-        data = 7
+        data = '7'
     elif 4 in array and 3 in array and len(array) == 2:
-        data = 8
+        data = '8'
     else:
-        data = 0
+        data = '0'
+
+    return data
